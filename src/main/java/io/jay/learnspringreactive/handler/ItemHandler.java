d184bbcddc899d6ada781882d77332c1c14e87bf
jayaramvsa <jayaramvsa@gmail.com>
2021-05-07 18:25:58 +0530

    Added use for ROuter and Handler for Item

---
+++ src/main/java/io/jay/learnspringreactive/handler/ItemHandler.java
@@ -0,0 +1,24 @@
+package io.jay.learnspringreactive.handler;
+
+import io.jay.learnspringreactive.document.Item;
+import io.jay.learnspringreactive.repository.ItemReactiveRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+public class ItemHandler {
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    public Mono<ServerResponse> getAllItems(ServerRequest serverRequest){
+
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(itemReactiveRepository.findAll(), Item.class);
+    }
+}
---
+++ src/main/java/io/jay/learnspringreactive/router/ItemsRouter.java
@@ -0,0 +1,25 @@
+package io.jay.learnspringreactive.router;
+
+import io.jay.learnspringreactive.handler.ItemHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static io.jay.learnspringreactive.constants.ItemConstants.ITEM_FUNCTIONAL_END_POINT_V1;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+
+
+@Configuration
+public class ItemsRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> itemsRoute(ItemHandler itemHandler) {
+
+        return RouterFunctions.route(GET(ITEM_FUNCTIONAL_END_POINT_V1).
+                and(accept(MediaType.APPLICATION_JSON)), itemHandler::getAllItems);
+    }
+}
